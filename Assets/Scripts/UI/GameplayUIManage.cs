afd56bb9f5b85913e67b7a69b6ac066f9ba51407
SpartaqS <gsrodriguez@wp.pl>
2019-05-08 13:58:52 +0200

    Wersja 0

---
+++ Assets/Scripts/UI/GameplayUIManage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayUIManage : MonoBehaviour
+{
+    public Button switchSceneButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switchSceneButton.onClick.AddListener(ClickAction);
+    }
+
+    void ClickAction()
+    {
+        Debug.Log("I AM BUTTON");
+    }
+}
