59b8c1c3b1a4a52e1c49e48959df0e4be580e30a
SpartaqS <gsrodriguez@wp.pl>
2019-05-12 17:03:50 +0200

    Wersja 5
    Wszystko co konieczne: zrobione

--- Assets/Scripts/UI/MainMenuUIManager.cs
+++ Assets/Scripts/UI/MainMenuUIManager.cs
@@ -5,14 +5,23 @@ using UnityEngine.SceneManagement;
 public class MainMenuUIManager : MonoBehaviour
 {
     public Button playButton;
+    public GameObject highscoreDisplay;
+    public Button resetHighscoreButton;
 
     void Start()
     {
+        highscoreDisplay.GetComponent<HighscoreDisplay>().CheckHighscore();
         playButton.onClick.AddListener(ClickAction);
+        resetHighscoreButton.onClick.AddListener(ClickActionReset);
     }
 
     void ClickAction()
     {
         SceneManager.LoadScene("Gameplay");
+    }
+    void ClickActionReset()
+    {
+        ScoreSavingSysytem.SaveHighscore(-1f);
+        highscoreDisplay.GetComponent<HighscoreDisplay>().CheckHighscore();
     }
 }
